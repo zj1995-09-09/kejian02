b5b913d7a09c00bbdf62760a5087748504db87d2
zhoujian <1587453282@qq.com>
2022-07-14 15:59:57 +0800

    initial commit2

---
+++ lesson03_data_type_list (2)/01domo04_format.py
@@ -0,0 +1,16 @@
+'''
+字符串格式化
+'''
+
+name ='周建'
+money =10000000
+money_chinese ='一千万'
+
+ticket ='''
+今收到{}
+交来的{}
+人民币{}
+
+'''.format(name,money,money_chinese)
+print(ticket)
+print(name +str(100))
